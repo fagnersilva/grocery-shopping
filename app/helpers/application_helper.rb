2414f361bdd971e77dc7123ac44047a60a92e220
Fagner Silva <p000fagsilva@prservicos.com.br>
2019-02-09 15:36:29 -0200

    create services

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+        def data_br(data_us) 
+        data_us.strftime("%d/%m/%Y")
+    end
+
+    def ambiente_rails
+        if Rails.env.development?
+            "Desenvolvimento"
+        elsif Rails.env.Production?
+            "Produção"
+        else
+            "Teste"
+        end       
+    end
 end
